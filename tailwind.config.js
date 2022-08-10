bf58c8e61b8bc8a834cae458a13d4400b53f48ec
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-08-10 09:34:36 -0600

    added respondive design to tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = {
       colors: {
         primary: "#075985",
         secondary: "#94a3b8"
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
       }
     },
   },
